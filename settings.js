5fd2a7112cc58c6c97d82065e68ec49e1e248564
Alexey Avdeev <aad.jerry@gmail.com>
2013-06-06 23:40:07 +0400

    settigs + index

---
+++ settings.js
@@ -0,0 +1,11 @@
+// количество лиц для вывода
+facesCount = 8;
+
+// номера лиц на которых нужно остановиться, начиная с единицы
+facesToChoose = [{
+  number: 4,
+  video: 'video_4.mp4'
+}, {
+  number: 1,
+  video: 'video_1.mpg'
+}];
